ea48bf8e4d13b05a3b353bfad670559541b06d84
Gavin <gurven-mckruger@hotmail.co.uk>
2024-07-20 10:41:46 +0100

    Updated gallery with image opt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://gavinmcgregor-2024.netlify.app/',
+	assets: true,
+	output: 'server',
+	adapter: netlify({
+		imageCDN: true
+	}),
+	image: {
+		service: passthroughImageService()
+	}
+})
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-module.exports = {
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		publicPath: '/'
-	}
-}
